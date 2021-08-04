046508832739fc7c124cc1468a4aa6bc19d1269e
Jonny Rojas <jrr1047@gmail.com>
2021-08-03 21:21:29 -0500

    breadcrumb component is added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './components/search/search.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 
 const components = [
   SearchComponent,
+  BreadcrumbComponent,
 ];
 
 @NgModule({
   declarations: [
-    ...components
+    ...components,
   ],
   exports: [
     ...components
